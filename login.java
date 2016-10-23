81e33c8d47d3b79ccf7ef1cae9562e300db2110c
veninababan <veninababan.vn@gmail.com>
2016-10-23 22:09:18 +0700

    Add files via upload
    add file login.java

---
+++ login.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class scanning {
+    public static void main(String[] args) {
+        String user1, password1, user, password;
+
+        //data loginnya
+        user1 = "abc";
+        password1 = "def";
+
+        //scanner untuk meng-input data yang akan di-cek
+        Scanner abc = new Scanner(System. in );
+
+        //formulir
+        System.out.println("masukkan Username: ");
+        user = abc.nextLine();
+        System.out.println("masukkan Password: ");
+        password = abc.nextLine();
+
+        //pilihan yang diberikan
+        if (user1.equals(user) && password1.equals(password)) {
+            System.out.println("login berhasil");
+        } else {
+            System.out.println("login gagal");
+        }
+    }
+}
